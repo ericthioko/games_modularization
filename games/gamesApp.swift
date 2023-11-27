d5ebed60b9a16038e8e4897fd3124de4700062dc
dtkid8 <erictheo8@gmail.com>
2023-11-27 19:03:07 +0800

    Initial Commit

---
+++ games/gamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  gamesApp.swift
+//  games
+//
+//  Created by Eric Theo on 27/11/23.
+//
+
+import SwiftUI
+
+@main
+struct gamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
